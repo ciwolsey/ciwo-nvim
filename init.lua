54f451f8fdca00b35b61adad99b02d626bb7e39c
ciwolsey <carl@wolsey.org>
2022-09-19 15:55:26 +0100

    Use rust-tools for rust and lspconfig for other languages

--- init.lua
+++ init.lua
@@ -10,6 +10,7 @@ vim.g.netrw_liststyle = 3
 -- Remove banner from top
 vim.g.netrw_banner = 0
 
+require("lspconfig-conf")
 require("neovide-conf")
 require("sets-conf")
 require("treesitter-conf")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,22 +2,23 @@ return require('packer').startup(function()
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
     use 'arcticicestudio/nord-vim'
+	-- LSPConfig
     use 'neovim/nvim-lspconfig'
-    use 'simrat39/rust-tools.nvim'
+	use 'simrat39/rust-tools.nvim'
 
     -- Optional dependencies
     use 'nvim-lua/popup.nvim'
     use 'nvim-lua/plenary.nvim'
     use 'nvim-telescope/telescope.nvim'
-
+	
     use 'mfussenegger/nvim-dap'
 
     -- cmp
+    use 'hrsh7th/nvim-cmp'
     use 'hrsh7th/cmp-nvim-lsp'
     use 'hrsh7th/cmp-buffer'
     use 'hrsh7th/cmp-path'
     use 'hrsh7th/cmp-cmdline'
-    use 'hrsh7th/nvim-cmp'
     use 'hrsh7th/cmp-vsnip'
     use 'hrsh7th/vim-vsnip'
 
